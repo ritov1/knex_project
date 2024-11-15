bd38a7dadbe312fc4adc06ab2af16d41d7e76738
ritov1 <ritozzz7@gmail.com>
2024-11-15 16:06:32 -0300

    add components

---
+++ src/components/price.tsx
@@ -0,0 +1,15 @@
+<div className="price">
+          <div className="title-area-conteiner-2">
+            <div className="title-area-content-2">
+              <div className="frame-2">
+                <p className="div-2">
+                  <span className="span">Payment</span>
+                  <span className="text-wrapper-8">&nbsp;</span>
+                  <span className="span">Methods</span>
+                </p>
+                <div className="text-wrapper-9">We keep it simple.</div>
+              </div>
+            </div>
+            <img className="rectangle" src="img/rectangle-1.png" alt="Rectangle" />
+          </div>
+        </div>
---
+++ src/components/process.tsx
@@ -0,0 +1,21 @@
+        <div className="process">
+          <div className="feature-card-2">
+            <div className="frame-12"></div>
+            <div className="frame-13">
+              <div className="frame-14"><div className="text-wrapper-12">Data Gathering</div></div>
+              <p className="we-need-a-thorough">
+                We need a thorough understanding of your business goals and needs.
+              </p>
+            </div>
+            <div className="icon-6"></div>
+          </div>
+          {/* Additional process cards */}
+          <div className="frame-15">
+            <p className="div-2">
+              <span className="span">Our</span>
+              <span className="text-wrapper-8">&nbsp;</span>
+              <span className="span">Workflow</span>
+            </p>
+            <p className="text-wrapper-9">Simple version of our workflow.</p>
+          </div>
+        </div>
